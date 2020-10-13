8d862eb0f2b9076841d25a2e3d3b32d0dc0a165d
NicolasFujisawa <nicolas.fujisawa@fatec.sp.gov.br>
2020-10-13 19:04:40 -0300

    add routes, page landing e page orphanates

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import logoImg from '../images/Logo.svg'
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import '../styles/pages/landing.css';
+
+function Landing() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <Link to="" className="enter-landing">
+          <img src={logoImg} alt="Happy"/>
+        </Link>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São José dos Campos</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight 
+            className="font-left"
+            size={26} 
+            color="rgba(0, 0, 0, 0.6)"
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import mapMarker from '../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'
+
+function OrphanageMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={ mapMarker } alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>
+            São Paulo
+          </strong>
+          <span>
+            São José dos Campos
+          </span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[-23.1935702,-45.8786849]} 
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={ 32 } color="#FFF"/>
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanageMap;
